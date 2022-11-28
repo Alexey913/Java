59308ab35d4897a1c8d9d714a5c36f5880e5520a
Alexey <al913@mail.ru>
2022-11-28 22:34:16 +0300

    Seminar 1 Final

--- Homework1/hw1_4.java
+++ Homework1/hw1_4.java
@@ -2,232 +2,131 @@
 // Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69.
 // Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.*/
 
-// import java.util.Scanner;
-// import java.lang.Math;
-// public class hw1_4
-// {
-//     public static void main(String[] args) {
-// 	    System.out.println ("Введите через пробел выражение для подсчета и нажмите Enter\n(Например, 2? + ?7 = 69)");
-// 	    Scanner enterData = new Scanner(System.in);
-//         String data = enterData.nextLine();
-//         String [] data_elem = data.split(" ");
-//         StringBuilder temp_1 = new StringBuilder(data_elem[0]);
-//         StringBuilder temp_2 = new StringBuilder(data_elem[2]);
-//         StringBuilder temp_3 = new StringBuilder(data_elem[4]);
-//         int len_1 = temp_1.length();
-//         int len_2 = temp_2.length();
-//         int len_3 = temp_3.length();
-//         int maxLen = Math.max(Math.max(len_1, len_2), len_3);
-//         System.out.println (maxLen);
-//         temp_1 = transformToMaxLen (maxLen, len_1, temp_1);
-//         temp_2 = transformToMaxLen (maxLen, len_2, temp_2);
-//         temp_3 = transformToMaxLen (maxLen, len_3, temp_3);
-//         System.out.println (temp_1);
-//         // int dig = Integer.parseInt(temp_1.toString());
-//         // System.out.println (dig);
-
-//         for (int i = 0; i < maxLen*3; i++) {
-//             if (temp_1.indexOf("?") == i) {
-//                 for (int j = 0; j < 10; j++) {
-//                     repla
-
-//                     }
-//                 }
-//             }
-//         }
-
-
-
-//         }
-
-//     static StringBuilder transformToMaxLen (int maxLen, int len, StringBuilder data) {
-//         if (len < maxLen) {
-//             int count = maxLen - len;
-//             while (count>0) {
-//                 data = data.insert(0, "0");
-//                 count--;
-//             }
-//         }
-//         return data;
-//     }
-// }
-
-
-
-
-
-/* *+Задано уравнение вида q + w = e, q, w, e >= 0. 
-Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69.
-Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.*/
-
 import java.util.Scanner;
 import java.lang.Math;
-import java.io.*;
 public class hw1_4
 {
     public static void main(String[] args) {
-	    System.out.println ("Введите через пробел выражение для подсчета и нажмите Enter\n(Например, 2? + 7? = 69)");
-	    Scanner enterData = new Scanner(System.in);
-        String data = enterData.nextLine();
-        String [] data_elem = data.split(" ");
-        String [] temp_1 = data_elem[0].split("");
-        String [] temp_2 = data_elem[2].split("");
-        String [] temp_3 = data_elem[4].split("");
-        int maxLen = Math.max(Math.max(temp_1.length, temp_2.length), temp_3.length);
-        String [] numOne = tempToArr (maxLen, temp_1);
-        String [] numTwo = tempToArr (maxLen, temp_2);
-        String [] numThree = tempToArr (maxLen, temp_3);
-        for (int i = maxLen-1; i >= 0; i--) {
-            int tempDigOne = 0;
-            int tempDigTwo = 0;
-            int tempDigThree = 0;
-            // int jStart=0, kStart=0, mStart=0;
-            // if (numOne[i].equals("0")) {
-            //     jStart = 1;
-            // }
-            // else {
-            //     jStart = 0;
-            // }
-            // if (numTwo[i].equals("0")) {
-            //     kStart = 1;
-            // }
-            // else {
-            //     kStart = 0;
-            // }
-            // if (numThree[i].equals("0")) {
-            //     mStart = 1;
-            // }
-            // else {
-            //     mStart = 0;
-            // }
-            // int charge = 0;
-            for (int j = 9; j <= 0; j--) {
-                if (!(numOne[i].equals("?"))) {
-                    tempDigOne = Integer.parseInt(numOne[i]);
-                    // System.out.printf("tempDigOne %d = %d\n", i, tempDigOne);
-                }
-                else {
-                    tempDigOne = j;
-                    // System.out.printf("tempDigOne %d = %d\n", i, tempDigOne);
-                }
-                for (int k = 9; k <= 0; k--) {
-                    if (!(numTwo[i].equals("?"))) {
-                        tempDigTwo = Integer.parseInt(numTwo[i]);
-                        // System.out.printf("tempDigTwo %d = %d\n", i, tempDigTwo);
-                    }
-                    else {
-                    tempDigTwo = k;
-                    // System.out.printf("tempDigTwo %d = %d\n", i, tempDigTwo);
-                    }
-                    for (int m = 9; m <= 0; m--) {                        
-                        if (!(numThree[i].equals("?"))) {
-                            tempDigThree = Integer.parseInt(numThree[i]);
-                            // System.out.printf("tempDigThree %d = %d\n", i, tempDigThree);
-                        }
-                        else {
-                            tempDigThree = m;
-                            // System.out.printf("tempDigThree %d = %d\n", i, tempDigThree);
-                        }
-                        if ((tempDigOne + tempDigTwo) == tempDigThree) {
-                            numOne[i] = String.valueOf(tempDigOne);
-                            // System.out.printf("\n"+numOne[i]);
-                            numTwo[i] = String.valueOf(tempDigTwo);
-                            // System.out.printf("\n"+numTwo[i]);
-                            // if ((Integer.parseInt(numOne[i+1]) + Integer.parseInt(numTwo[i+1]))%10 == (Integer.parseInt(numOne[i+1]) + Integer.parseInt(numTwo[i+1]))) {
-                            numThree[i] = String.valueOf(tempDigThree);
-                            // }
-                            // else {
-                            //     numThree[i] = String.valueOf(tempDigThree+1);
-                            // }
-                            // System.out.printf("\n"+numThree[i]);
-                            // if (((tempDigOne + tempDigTwo)%10) < (tempDigOne + tempDigTwo)) {
-                            //     charge = 0;
-                            // }
-                            // else {
-                            //     charge = 1;
-                            // }
-                            // break;
-                        }
-                    }
-                }
+        int excep = 0;
+        while (excep == 0) {
+            try{
+                excep = main_menu();
             }
-        }
-        for (String i: numOne) {
-            System.out.printf(""+i+"");
-        }
-        System.out.println();
-        for (String i: numTwo) {
-            System.out.printf(""+i+"");
-        }
-        System.out.println();
-        for (String i: numThree) {
-            System.out.printf(""+i+"");
+            catch (NumberFormatException e) {
+                System.out.println ("Ошибка ввода! Повторите! \n");
+                excep = 0;
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println ("Ошибка ввода! Повторите! \n");
+                excep = 0;
+            }
         }
     }
 
-
-
-
-
-        //         try {
-        //             tempDigOne = Integer.parseInt (numOne[i]);
-        //         }
-        //         catch (NumberFormatException e) {
-        //             tempDigOne = j;
-        //         }
-        //         System.out.printf ("цифра первого числа %d\n", tempDigOne);
-        //         for (int k = 0; k <= 9; k++) {
-        //             try {
-        //                 tempDigTwo = Integer.parseInt (numTwo[i]);
-        //             }
-        //             catch (NumberFormatException e) {
-        //                 tempDigTwo = k;
-        //             }
-        //             System.out.printf ("цифра второго числа %d\n", tempDigTwo);
-        //             for (int m = 0; m <= 9; m++) {
-        //                 try {
-        //                     tempDigThree = Integer.parseInt (numThree[i]);
-        //                 }
-        //                 catch (NumberFormatException e) {
-        //                     tempDigThree = m;
-        //                 }
-        //                 System.out.printf ("цифра третьего числа %d\n", tempDigThree);
-        //                 if (tempDigOne + tempDigTwo == tempDigThree) {
-        //                     numOne[i] = String.valueOf(tempDigOne);
-        //                     numTwo[i] = String.valueOf(tempDigTwo);
-        //                     numThree[i] = String.valueOf(tempDigThree);
-        //                 }
-        //         }
-        //     }
-        // }
-        // }
-        //         double result_1 = Integer.parseInt (numOne[0]);
-        //         double result_2 = Integer.parseInt (numTwo[0]);
-        //         double result_3 = Integer.parseInt (numThree[0]);
-        //         for (int i = 1; i < numOne.length; i++) {
-        //             double degree = Math.pow(10,numOne.length-i-1);
-        //             double tempOne = Integer.parseInt(numOne[i]);
-        //             double tempTwo = Integer.parseInt(numTwo[i]);
-        //             double tempThree = Integer.parseInt(numThree[i]);
-        //             result_1+=tempOne*degree;
-        //             result_2+=tempTwo*degree;
-        //             result_3+=tempThree*degree;
-        //     System.out.printf ("%f + %f = %f", result_1, result_2, result_3);
     
+    static int main_menu() {
+        String [] elements = inputData();
+        while ((elements.length != 5)||(!elements[1].equals("+"))||(!elements[3].equals("="))) {
+            System.out.printf ("Ошибка ввода! Повторите! \n");
+            elements = inputData();
+        }
+        String [] temp_1 = elements[0].split("");
+        String [] temp_2 = elements[2].split("");
+        String [] temp_3 = elements[4].split("");
+        int [] numOne = numVariosy(temp_1, elements[0]);
+        int [] numTwo = numVariosy(temp_2, elements[2]);
+        int [] numThree = numVariosy(temp_3, elements[4]);
+        int excep = selectSol(numOne, numTwo, numThree);
+        return excep;
+    }
 
 
-    static String[] tempToArr(int len, String[] temp) {
-        String [] temp_array = new String [len];
-        int lenTemp = temp.length;
-        for (int i = 0; i<len; i++) {
-                if (i < len - lenTemp) {
-                    temp_array [i] = "0";
+    static String[] inputData() {
+	    System.out.println ("Введите через пробел выражение для подсчета и нажмите Enter\n(Например, 2? + ?7 = 69)");
+	    Scanner enterData = new Scanner(System.in);
+            String data = enterData.nextLine();
+            String [] data_elem = data.split(" ");
+            return data_elem;
+	}
+
+
+    static int [] numVariosy (String [] arr, String str) {
+        int count = 0;
+        int [] arrayPos = new int [arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("?")) {
+                count++;
+                arrayPos[i] = 1;
+            }
+            else {
+                arrayPos[i] = 0;
+            }
+        }
+        if (count == 0) {
+            int [] arrayNum = new int [] {Integer.parseInt(str)};
+            return arrayNum;
+        }
+        else {
+            int quant = (int) Math.pow(10, count);
+            String [] arrayStr = new String [quant];
+            String [] arrayNumFor = new String [quant];
+            for (int i = 0; i < quant; i++) {
+                arrayStr[i] = str;
+                int lenNum = String.valueOf(i).length();
+                int delta = count - lenNum;
+                if (lenNum<count) {
+                    arrayNumFor[i] = "0".repeat(delta)+String.valueOf(i);
                 }
                 else {
-                    temp_array [i] = temp[i - (len - lenTemp)];
+                    arrayNumFor[i] = String.valueOf(i);
                 }
-                // System.out.println (temp_array[i]);
             }
-        return temp_array;
-}
+            int count2 = 0;
+            for (int k = 0; k < arr.length; k++) {
+                if (arrayPos[k] == 1) {
+                    for (int j = 0; j<quant; j++) {
+                        char[] charsStart = arrayNumFor[j].toCharArray();
+                        char newChar = charsStart[count2];
+                        char[] charsFinish = arrayStr[j].toCharArray();
+                        charsFinish[k] = newChar;
+                        arrayStr[j] = String.valueOf(charsFinish);
+                    }
+                    count2++;
+                }
+            }
+            int [] arrayNum = new int [quant];
+            for (int i = 0; i<quant; i++) {
+                arrayNum[i] = Integer.parseInt(arrayStr[i]);
+            }
+        return arrayNum;
+        }
+    }
+
+    // public static boolean isNumeric(String str) { 
+    //     try {  
+    //         Integer.parseInt(str);
+    //         return true;
+    //     }
+    //     catch(NumberFormatException e){  
+    //         return false;  
+    //     }  
+    // }
+
+    public static int selectSol (int [] arr1, int [] arr2, int [] arr3) {
+        int count = 1;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                for (int k = 0; k < arr3.length; k++) {
+                    if (arr1[i] + arr2[j] == arr3[k]) {
+                        System.out.printf("Решение %d: %d + %d = %d\n", count, arr1[i], arr2[j], arr3[k]);
+                        count++;
+                    }
+                }
+            }
+        }
+        if (count == 1) {
+            System.out.printf("Решений нет!");
+        }
+        return 1;
+    }
 }
