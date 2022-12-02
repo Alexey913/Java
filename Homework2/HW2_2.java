06158bd98d59e7593d3cb0cc3cb5f5fbc95bd44c
Alexey <108893923+Alexey913@users.noreply.github.com>
2022-12-02 15:32:27 +0300

    Update HW2_2.java

--- Homework2/HW2_2.java
+++ Homework2/HW2_2.java
@@ -33,20 +33,21 @@ public class Main {
             excep = main_menu();
         }
         catch (NumberFormatException e) {
-            System.out.println ("Ошибка ввода! Повторите! \n");
+            System.out.println ("\tОшибка ввода! Повторите! >>>\n");
             loging (2, "Ошибка ввода! Неверный формат!\n");
         }
         catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println ("Ошибка ввода! Повторите! \n");
+            System.out.println ("\tОшибка ввода! Повторите! >>>\n");
             loging (2, "Ошибка ввода! Неверное число элементов!\n");
         }
     }
 }
 
 	static String[] inputData() throws SecurityException, IOException {
-	    System.out.println ("Введите через пробел выражение для подсчета и нажмите Enter\n(Например, 8.5 + 10)\n");
+	    System.out.println ("\t Введите через пробел выражение для подсчета и нажмите Enter\n\t(Например, 8.5 + 10) >>>");
 	    Scanner enterData = new Scanner(System.in);
         String data = enterData.nextLine();
+        System.out.println();
         String [] data_elem = data.split(" ");
         loging (1,  "Введено значение для подсчёта " + data + "\n");
         return data_elem;
@@ -56,33 +57,33 @@ public class Main {
 	    switch (symbol) {
             case "+":
                 loging (1,  "Запуск операции сложения\n");
-                System.out.printf ("Сумма чисел %f и %f равна %f\n", numOne,numTwo, numOne+numTwo);
+                System.out.printf ("\tСумма чисел %f и %f равна %f\n\n", numOne,numTwo, numOne+numTwo);
                 loging (1,  "Успешное сложение\n");
                 break;
             case "-":
-                // loging (2,  "Запуск операции вычитания\n");
-                System.out.printf ("Разность чисел %f и %f равна %f\n", numOne,numTwo, numOne-numTwo);
+                loging (1,  "Запуск операции вычитания\n");
+                System.out.printf ("\tРазность чисел %f и %f равна %f\n\n", numOne,numTwo, numOne-numTwo);
                 loging (1,  "Успешное вычитание\n");
                 break;
             case "*":
                 loging (1,  "Запуск операции умножения\n");
-                System.out.printf ("Произведение чисел %f и %f равно %f\n", numOne,numTwo, numOne*numTwo);
+                System.out.printf ("\tПроизведение чисел %f и %f равно %f\n\n", numOne,numTwo, numOne*numTwo);
                 loging (1,  "Успешное умножение\n");
                 break;
             case "/":
                 if (numTwo != 0) {
                     loging (1,  "Запуск операции деления\n");
-                    System.out.printf ("Частное %f и %f равно %f\n", numOne,numTwo, numOne/numTwo);
+                    System.out.printf ("\tЧастное %f и %f равно %f\n\n", numOne,numTwo, numOne/numTwo);
                     loging (1,  "Успешное деление\n");
                 }
                 else {
                     loging (2,  "Ошибка запуска операции деления - деление на 0!\n");
-                    System.out.println ("Ошибка! На 0 делить нельзя!\n");
+                    System.out.println ("\tОшибка! На 0 делить нельзя!\n\n");
                 }
                 break;
             case "%":
                 loging (1,  "Запуск нахождения остатка от деления\n");
-                System.out.printf ("Остаток от деления числа %f на число %f равно %f\n", numOne,numTwo, numOne%numTwo);
+                System.out.printf ("\tОстаток от деления числа %f на число %f равно %f\n\n", numOne,numTwo, numOne%numTwo);
                 break;
             case "^":
                 float result = numOne;
@@ -96,20 +97,20 @@ public class Main {
                         result = 1/result;
                     }
                     if (numTwo == 0) result = 1;
-                    System.out.printf ("Число %f в степени %f равно %f\n", numOne,numTwo, result);
+                    System.out.printf ("\tЧисло %f в степени %f равно %f\n\n", numOne,numTwo, result);
                     loging (1,  "Успешное возведение в степень\n");
                 }
                 else {
                     loging (2,  "Ошибка запуска операции возведения в степень - степень не является целым числом!\n");
-                    System.out.println ("Ошибка! Степень должна быть целым числом!\n");
+                    System.out.println ("\tОшибка! Степень должна быть целым числом!\n\n");
                 }
                 break;
             default:
                 loging (2,  "Ошибка выбора действия. Действия " + symbol + " не предусмотрено!\n");
-                System.out.print ("Я не знаю такого действия!\n");
+                System.out.print ("\tЯ не знаю такого действия!\n\n");
                 break;
 	    }
-        System.out.printf ("Желаете еще что-нибудь посчитать? \n1 - Повторить операцию с числами %f и %f\n2 - Выполнить новую операцию\n3 - Выход\n", numOne, numTwo);
+        System.out.printf ("\tЖелаете еще что-нибудь посчитать? >>>\n\t\t1 - Повторить операцию с числами %f и %f\n\t\t2 - Выполнить новую операцию\n\t\t3 - Выход\n\n", numOne, numTwo);
         Scanner answer = new Scanner(System.in);
         loging (1,  "Запрос нового действия\n");
         String user_answ = answer.nextLine();
@@ -121,7 +122,7 @@ public class Main {
 	static int main_menu() throws SecurityException, IOException {
     	String [] elements = inputData();
         while (elements.length>3) {
-            System.out.printf ("Ошибка ввода! Повторите! \n");
+            System.out.printf ("\tОшибка ввода! Повторите! >>>\n");
             elements = inputData();
         }
         float num_1 = Float.parseFloat (elements[0]);
@@ -135,8 +136,9 @@ public class Main {
     switch (answ) {
         case "1":
             loging (1, "Предложение выбора действия с числами\n" + numOne + " и "+ numTwo);
-            System.out.println ("Какое действие будем использовать?\nВведите одно из предложенных действий\n+ - * / % ^\n");
+            System.out.println ("\tКакое действие будем использовать?\nВведите одно из предложенных действий >>>\n\n\t\t+ - * / % ^\n");
             Scanner act = new Scanner(System.in);
+            System.out.println ();
             String action = act.nextLine();
             loging (1, "Повторная операция с числами\n" + numOne + " и " + numTwo + " - " + action);
             int excep = calculator (numOne, numTwo, action);
@@ -147,11 +149,11 @@ public class Main {
             return excep;
         case "3":
             loging (1, "Выход по команде пользователя\n");
-            System.out.println ("Приходите еще!");
+            System.out.println ("\tПриходите еще!");
             return 1;
 	    default:
 	        loging (2, "Выход в результате неверного ввода пользователя\n");
-	        System.out.println ("Не понял Вас. Давайте попробуем в следующий раз!");
+	        System.out.println ("\tНе понял Вас. Давайте попробуем в следующий раз!");
 	        return 1;
         }
 	}
