1142cd1a5530f11c5238dc9113d47f42e8e50fd8
Alexey <108893923+Alexey913@users.noreply.github.com>
2022-12-02 13:09:27 +0300

    Update HW2_2.java

--- Homework2/HW2_2.java
+++ Homework2/HW2_2.java
@@ -2,19 +2,31 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.*;
 
-public class HW2_2 {
-    private static Logger log = Logger.getLogger(HW2_2.class.getName());
+public class Main {
+    
+    private static Logger log = Logger.getLogger(Main.class.getName());
+    
+    public static void loging (int switcher, String msg) throws SecurityException, IOException {
+        
+        Handler fh  = null;
+        fh = new FileHandler("HW2_2.txt" + "run.log",  1000000, 1, true);
+        log.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+        switch(switcher)
+            {
+            case 1:
+                log.log(Level.INFO, msg);
+                break;
+            case 2:
+                log.log(Level.WARNING, msg);
+                break;
+	    }
+        fh.close();
+    }
 
 	public static void main(String[] args) throws SecurityException, IOException {
-
-    Handler fh  = null;
-    fh = new FileHandler("HW2_2.txt" + "run.log",  1000000, 1, true);
-    // log.addHandler(fh);
-    SimpleFormatter sFormat = new SimpleFormatter();
-    fh.setFormatter(sFormat);
-    
-    log.log(Level.INFO, "Запуск калькулятора \n");
-    
+    loging (1, "Запуск программы\n");
     int excep = 0;
     while (excep == 0) {
         try{
@@ -22,55 +34,59 @@ public class HW2_2 {
         }
         catch (NumberFormatException e) {
             System.out.println ("Ошибка ввода! Повторите! \n");
-            log.log(Level.WARNING, "Ошибка ввода! Неверный формат!\n");
+            loging (2, "Ошибка ввода! Неверный формат!\n");
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println ("Ошибка ввода! Повторите! \n");
-            log.log(Level.WARNING, "Ошибка ввода! Неверное число элементов!\n");
+            loging (2, "Ошибка ввода! Неверное число элементов!\n");
         }
     }
 }
-        
+
 	static String[] inputData() throws SecurityException, IOException {
-	    System.out.println ("Введите через пробел выражение для подсчета и нажмите Enter\n(Например, 8.5 + 10)");
+	    System.out.println ("Введите через пробел выражение для подсчета и нажмите Enter\n(Например, 8.5 + 10)\n");
 	    Scanner enterData = new Scanner(System.in);
         String data = enterData.nextLine();
         String [] data_elem = data.split(" ");
-        
-        // Logger log = Logger.getLogger(HW2_1.class.getName());
-        FileHandler fh = new FileHandler("HW2_2.txt" + "run.log", 1000000, 1, true);
-        log.addHandler(fh);
-        SimpleFormatter sFormat = new SimpleFormatter();
-        fh.setFormatter(sFormat);
-            
-        log.log(Level.INFO, "Введено значение " + data + "\n");
+        loging (1,  "Введено значение для подсчёта " + data + "\n");
         return data_elem;
 	}
 	
 	static int calculator (float numOne, float numTwo, String symbol) throws SecurityException, IOException {
 	    switch (symbol) {
             case "+":
-                System.out.printf ("Сумма чисел %f и %f равна %f", numOne,numTwo, numOne+numTwo);
+                loging (1,  "Запуск операции сложения\n");
+                System.out.printf ("Сумма чисел %f и %f равна %f\n", numOne,numTwo, numOne+numTwo);
+                loging (1,  "Успешное сложение\n");
                 break;
             case "-":
-                System.out.printf ("Разность чисел %f и %f равна %f", numOne,numTwo, numOne-numTwo);
+                // loging (2,  "Запуск операции вычитания\n");
+                System.out.printf ("Разность чисел %f и %f равна %f\n", numOne,numTwo, numOne-numTwo);
+                loging (1,  "Успешное вычитание\n");
                 break;
             case "*":
-                System.out.printf ("Произведение чисел %f и %f равно %f", numOne,numTwo, numOne*numTwo);
+                loging (1,  "Запуск операции умножения\n");
+                System.out.printf ("Произведение чисел %f и %f равно %f\n", numOne,numTwo, numOne*numTwo);
+                loging (1,  "Успешное умножение\n");
                 break;
             case "/":
                 if (numTwo != 0) {
-                    System.out.printf ("Частное %f и %f равно %f", numOne,numTwo, numOne/numTwo);
+                    loging (1,  "Запуск операции деления\n");
+                    System.out.printf ("Частное %f и %f равно %f\n", numOne,numTwo, numOne/numTwo);
+                    loging (1,  "Успешное деление\n");
                 }
                 else {
-                    System.out.println ("Ошибка! На 0 делить нельзя!");
+                    loging (2,  "Ошибка запуска операции деления - деление на 0!\n");
+                    System.out.println ("Ошибка! На 0 делить нельзя!\n");
                 }
                 break;
             case "%":
-                System.out.printf ("Остаток от деления числа %f на число %f равно %f", numOne,numTwo, numOne%numTwo);
+                loging (1,  "Запуск нахождения остатка от деления\n");
+                System.out.printf ("Остаток от деления числа %f на число %f равно %f\n", numOne,numTwo, numOne%numTwo);
                 break;
             case "^":
                 float result = numOne;
+                loging (1,  "Запуск операции возведения в степень\n");
                 if (numTwo%1 == 0) {
                     if (numTwo>0) {
                         for (int i = 1; i < numTwo; i++) result*=numOne;
@@ -80,18 +96,22 @@ public class HW2_2 {
                         result = 1/result;
                     }
                     if (numTwo == 0) result = 1;
-                    System.out.printf ("Число %f в степени %f равно %f", numOne,numTwo, result);
+                    System.out.printf ("Число %f в степени %f равно %f\n", numOne,numTwo, result);
+                    loging (1,  "Успешное возведение в степень\n");
                 }
                 else {
-                    System.out.println ("Ошибка! Степень должна быть целым числом!");
+                    loging (2,  "Ошибка запуска операции возведения в степень - степень не является целым числом!\n");
+                    System.out.println ("Ошибка! Степень должна быть целым числом!\n");
                 }
                 break;
             default:
-                System.out.print ("Я не знаю такого действия!");
+                loging (2,  "Ошибка выбора действия. Действия " + symbol + " не предусмотрено!\n");
+                System.out.print ("Я не знаю такого действия!\n");
                 break;
 	    }
-        System.out.printf ("\n\nЖелаете еще что-нибудь посчитать? \n1 - Повторить операцию с числами %f и %f\n2 - Выполнить новую операцию\n3 - Выход\n", numOne, numTwo);
+        System.out.printf ("Желаете еще что-нибудь посчитать? \n1 - Повторить операцию с числами %f и %f\n2 - Выполнить новую операцию\n3 - Выход\n", numOne, numTwo);
         Scanner answer = new Scanner(System.in);
+        loging (1,  "Запрос нового действия\n");
         String user_answ = answer.nextLine();
         int excep = repeat_operation(user_answ, numOne, numTwo);
         return excep;
@@ -114,18 +134,23 @@ public class HW2_2 {
 	static int repeat_operation (String answ, float numOne, float numTwo) throws SecurityException, IOException {
     switch (answ) {
         case "1":
+            loging (1, "Предложение выбора действия с числами\n" + numOne + " и "+ numTwo);
             System.out.println ("Какое действие будем использовать?\nВведите одно из предложенных действий\n+ - * / % ^\n");
             Scanner act = new Scanner(System.in);
             String action = act.nextLine();
+            loging (1, "Повторная операция с числами\n" + numOne + " и " + numTwo + " - " + action);
             int excep = calculator (numOne, numTwo, action);
             return excep;
         case "2":
+            loging (1, "Новый ввод данных\n");
             excep = main_menu();
             return excep;
         case "3":
+            loging (1, "Выход по команде пользователя\n");
             System.out.println ("Приходите еще!");
             return 1;
 	    default:
+	        loging (2, "Выход в результате неверного ввода пользователя\n");
 	        System.out.println ("Не понял Вас. Давайте попробуем в следующий раз!");
 	        return 1;
         }
