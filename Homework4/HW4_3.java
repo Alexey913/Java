c2324c118d0cec29c47c225a6ea60ed8c26074e5
Alexey <al913@mail.ru>
2022-12-04 17:03:04 +0300

    seminar 4

---
+++ Homework4/HW4_3.java
@@ -0,0 +1,65 @@
+/* Написать программу, определяющую правильность расстановки скобок в выражении.
+
+a+(d*3) - истина
+\[a+(1*3) - ложь
+\[6+(3*3)] - истина
+{a}\[+\]{(d*3)} - истина
+<{a}+{(d*3)}> - истина
+{a+]}{(d*3)} - ложь */
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class HW4_3 {
+    public static void main(String[] args) {
+        Stack <String> inList = new Stack<>();
+        System.out.println("Введите выражение для проверки:");
+        Scanner sc = new Scanner(System.in);
+        String [] forList = sc.nextLine().split("");
+        String [] symbols = new String [] {"(", "[", "{", "<", ")", "]", "}", ">"};
+        boolean result = true;
+        int [] check = new int [symbols.length];
+        int [] count = new int [symbols.length/2];
+        for (int i = 0; i < forList.length; i++) {
+            inList.add(forList[i]);
+        }
+        while (!inList.isEmpty()) {
+            for (int i = 0; i < symbols.length/2; i++) {
+                if (inList.firstElement().equals(symbols[i])) {
+                    check[i]++;
+                }
+                else if (inList.firstElement().equals(symbols[i+symbols.length/2])) {
+                    check[i+symbols.length/2]++;
+                    count[i]--; // попробовать через MAP (типа словаря сделать, и под каждую скобку добавлять или убавлять 1)
+                }
+                System.out.println(check[i] + "-" + symbols[i] + "; " + check[i+symbols.length/2]+ "-" + symbols[i+symbols.length/2] +"; " + count[i]);
+            }
+            for (int j = 1; j < count.length; j++) {
+                if ((count[j]<count[j-1])) {
+                    result = false;
+                    break;
+                }
+            }
+            System.out.println("----------");
+            // System.out.println(result);
+            inList.remove(inList.firstElement());
+        }
+
+        if (result==true) {
+            for (int i = 0; i < check.length/2; i++) {
+                if (check[i]==check[i+symbols.length/2]) {
+                    result = true;
+                }
+                else {
+                    result = false;
+                    break;
+                }
+            }
+        }
+// for (int i = 0; i < check.length; i++) {
+//     System.out.println(symbols[i] + ": " + check[i]);
+// }
+        System.out.println(result);
+    }
+}
